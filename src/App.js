aae5ba2cb0ed05e0f5afc7afe39f544a1ff3eb96
Tim Ings <tim@tim-ings.com>
2020-05-13 16:13:35 +0800

    Moves firebase consumer to SignUp module

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import SignUpPage from './components/SignUp';
-import { FirebaseContext } from './components/Firebase';
 import './theme.scss';
 
 function App() {
     return (
         <div className="App">
-            <FirebaseContext.Consumer>
-                {firebase => <SignUpPage firebase={firebase} />}
-            </FirebaseContext.Consumer>
+            <SignUpPage />
         </div>
     );
 }
--- src/components/SignUp/SignUpPage.jsx
+++ src/components/SignUp/SignUpPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignUpForm from './SignUpForm';
+import { FirebaseContext } from '../Firebase';
 import './SignUpPage.scss';
 
-const SignUpPage = ({ firebase }) => (
+const SignUpPage = () => (
     <div className="signup-container">
         <h1>Sign Up</h1>
-        <SignUpForm firebase={firebase} />
+        <FirebaseContext.Consumer>
+            {firebase => <SignUpForm firebase={firebase} />}
+        </FirebaseContext.Consumer>
     </div>
 );
 
