836173d99c5d9ece691b752546b32fd0d075d106
Tim Ings <tim@tim-ings.com>
2020-05-14 12:37:06 +0800

    Adds redirect on successful sign in/up

--- src/components/SignIn/SignInPage.jsx
+++ src/components/SignIn/SignInPage.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import SignInForm from './SignInForm';
 import * as ROUTES from '../../routes';
 
-const SignInPage = () => (
-    <div className="card">
-        <div className="card-top">
-            <h1>Sign In</h1>
+const SignInPage = () => {
+    const auth = useSelector((state) => state.firebase.auth);
+
+    if (auth.uid) { // redirect to landing after successful sign in
+        return <Redirect to={ROUTES.LANDING} />;
+    }
+
+    return (
+        <div className="card">
+            <div className="card-top">
+                <h1>Sign In</h1>
+            </div>
+            <div className="card-bottom">
+                <SignInForm />
+                <p>
+                    Don&apos;t have an account?
+                    {' '}
+                    <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+                    .
+                </p>
+            </div>
         </div>
-        <div className="card-bottom">
-            <SignInForm />
-            <p>
-                Don&apos;t have an account?
-                {' '}
-                <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-                .
-            </p>
-        </div>
-    </div>
-);
+    );
+};
 
 export default SignInPage;
--- src/components/SignUp/SignUpPage.jsx
+++ src/components/SignUp/SignUpPage.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 import * as ROUTES from '../../routes';
 
-const SignUpPage = () => (
-    <div className="card">
-        <div className="card-top">
-            <h1>Sign Up</h1>
+const SignUpPage = () => {
+    const auth = useSelector((state) => state.firebase.auth);
+
+    if (auth.uid) { // redirect to landing after successful sign up
+        return <Redirect to={ROUTES.LANDING} />;
+    }
+
+    return (
+        <div className="card">
+            <div className="card-top">
+                <h1>Sign Up</h1>
+            </div>
+            <div className="card-bottom">
+                <SignUpForm />
+                <p>
+                    Already have an account?
+                    {' '}
+                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                    .
+                </p>
+            </div>
         </div>
-        <div className="card-bottom">
-            <SignUpForm />
-            <p>
-                Already have an account?
-                {' '}
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-                .
-            </p>
-        </div>
-    </div>
-);
+    );
+};
 
 export default SignUpPage;
